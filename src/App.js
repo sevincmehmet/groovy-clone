f160226edb6a7a1f4331d190d1065127e10788e7
Mehmet Ali <kemalist611661@gmail.com>
2023-03-06 20:24:58 +0300

    navbar  tamamlandı

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Main from './component/Main';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/component/Main.js
@@ -0,0 +1,15 @@
+import Navbars from "./Navbars";
+import "./Main.css"
+
+const Main = () => {
+    return(
+        <>
+            <div className="container-xl main-cont">
+                <Navbars />
+            </div>
+            
+        </>
+    )
+}
+
+export default Main;
